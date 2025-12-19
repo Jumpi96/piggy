65abd6ec449681c4db6def302ac96f696b896453
Jumpi96 <juampilorenzo@gmail.com>
2025-12-19 11:47:48 -0300

    feat: clean legacy Go app

--- legacy/deploy_lambda.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-GOOS=linux go build -o main main.go
-zip deployment.zip main 
-aws lambda update-function-code --function-name Piggy --zip-file fileb://deployment.zip 
-rm deployment.zip main
