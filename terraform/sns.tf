6ad32d45969f316e9e775cf257f9b073e52bd2ef
Jumpi96 <juampilorenzo@gmail.com>
2026-01-06 11:45:18 -0300

    fix: removing lambda

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -29,22 +29,3 @@ resource "aws_cloudwatch_metric_alarm" "backup_lambda_errors" {
   alarm_actions = [aws_sns_topic.lambda_alerts.arn]
 }
 
-# CloudWatch Alarm for Recurring Generator Lambda Errors
-resource "aws_cloudwatch_metric_alarm" "recurring_lambda_errors" {
-  alarm_name          = "piggy-recurring-lambda-errors"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 1
-  metric_name         = "Errors"
-  namespace           = "AWS/Lambda"
-  period              = 300
-  statistic           = "Sum"
-  threshold           = 0
-  alarm_description   = "Alert when recurring generator lambda fails"
-  treat_missing_data  = "notBreaching"
-
-  dimensions = {
-    FunctionName = aws_lambda_function.recurring_generator.function_name
-  }
-
-  alarm_actions = [aws_sns_topic.lambda_alerts.arn]
-}
